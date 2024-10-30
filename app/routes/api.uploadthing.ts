9eb7cbf8a53ab02d6474ee10b2ee4188f780820d
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-10-30 18:00:50 +0530

    added uploadthing

---
+++ app/routes/api.uploadthing.ts
@@ -0,0 +1,50 @@
+import type { ActionFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
+
+import { createUploadthing, type FileRouter } from "uploadthing/remix";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const auth = (args: ActionFunctionArgs) => ({ id: "fakeId" }); // Fake auth function
+
+// FileRouter for your app, can contain multiple FileRoutes
+const uploadRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ event }) => {
+      // This code runs on your server before upload
+      const user = await auth(event);
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+import { createRouteHandler } from "uploadthing/remix";
+
+const UploadRouter = {
+  // ...
+};
+
+export const { action, loader } = createRouteHandler({
+  router: uploadRouter,
+
+  // Apply an (optional) custom config:
+  // config: { ... },
+});
+
+
+export type UploadRouter = typeof uploadRouter;
---
+++ app/utils/uploadthing.ts
@@ -0,0 +1,10 @@
+import {
+    generateUploadButton,
+    generateUploadDropzone,
+  } from "@uploadthing/react";
+  
+  import type { UploadRouter } from "~/routes/api.uploadthing";
+  
+  export const UploadButton = generateUploadButton<UploadRouter>();
+  export const UploadDropzone = generateUploadDropzone<UploadRouter>();
+  
