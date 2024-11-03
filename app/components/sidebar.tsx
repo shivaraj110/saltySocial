2dd36c119749d6f602c0472951b3e3f1f1b61ab2
shivaraj110 <shivarajchandaragi9@gmail.com>
2024-11-03 19:28:38 +0530

    made the sidebar pretty

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 export default function () {
+  const [fold,setFold] = useState(true)
   return (
-    <div className="pl-5 pt-10 w-[215px] text-sm">
-      <div className="text-gray-500 flex cursor-pointer">
+    <div className={`  ${fold ? "w-[75px] ": "w-[215px] "}  transi text-sm`} >
+      <div className={` ${fold ? "hidden" : "flex"} pt-6 pr-3 text-gray-600 font-light cursor-pointer justify-end`}
+      onClick={
+        ()=>{
+          setFold(true)
+        }
+      }>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-gray-500 size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
+
+      </div>
+      <div className={` ${fold ? "flex":"hidden"} justify-center cursor-pointer  pt-6 gap-[2px] pb-[14px] mx-auto flex-row`}
+      onClick={()=>{
+        setFold(false)
+      }}
+      >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-gray-500 size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
+
+      </div>
+  <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -15,10 +39,14 @@ export default function () {
             d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
           />
         </svg>
-        <div className=" translate-y-[2px] font-semibold pl-3">Feed</div>
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Feed</div>
       </div>{" "}
-      <div className="text-gray-500 flex cursor-pointer mt-5">
-        <svg
+    
+      <div className=" hover:bg-slate-100 ">
+
+      </div>
+      <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
+      <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -32,10 +60,13 @@ export default function () {
           />
         </svg>
 
-        <div className=" translate-y-[2px] font-semibold pl-3">Friends</div>
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Friends</div>
       </div>{" "}
-      <div className="text-gray-500 flex cursor-pointer mt-5">
-        <svg
+      <div className=" hover:bg-slate-100 ">
+
+      </div>
+      <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
+      <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -49,9 +80,11 @@ export default function () {
           />
         </svg>
 
-        <div className=" translate-y-[2px] font-semibold pl-3">Event</div>
-      </div>{" "}
-      <div className="text-gray-500 flex cursor-pointer mt-5">
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Event</div>      </div>{" "}
+        <div className=" hover:bg-slate-100 ">
+
+        </div>
+        <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -66,12 +99,13 @@ export default function () {
           />
         </svg>
 
-        <div className=" translate-y-[2px] font-semibold pl-3">
-          Watch videos
-        </div>
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Watch Videos</div>
       </div>{" "}
-      <div className="text-gray-500 flex cursor-pointer mt-5">
-        <svg
+      <div className=" hover:bg-slate-100 ">
+
+      </div>
+      <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
+      <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -85,9 +119,11 @@ export default function () {
           />
         </svg>
 
-        <div className=" translate-y-[2px] font-semibold pl-3">Photos</div>
-      </div>{" "}
-      <div className="text-gray-500 flex cursor-pointer mt-5">
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Photos</div>      </div>{" "}
+        <div className=" hover:bg-slate-100 ">
+
+        </div>
+        <div className={`text-gray-500 flex cursor-pointer py-[10px] ${fold ? "justify-center" : " pl-6 justify-start"} hover:bg-slate-100 `} >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -102,32 +138,51 @@ export default function () {
           />
         </svg>
 
-        <div className=" translate-y-[2px] font-semibold pl-3">Files</div>
-      </div>
-      <div className="">
-        <div className="text-gray-400 pt-10 font-semibold">PAGES YOU LIKE</div>
+        <div className={` ${fold ? "hidden":"flex"} translate-y-[2px] font-semibold pl-3`} >Files</div>      </div>
+        <div className= {` ${fold ? "flex " : "hidden" } mx-auto mt-4 bg-slate-500 h-[1px] w-[25px]`}>
+
+        </div>
+      <div className="pt-6">
+        <div className= {` ${fold ? "hidden":"flex"} text-gray-400  pl-6 font-semibold`}  >PAGES YOU LIKE</div>
         <section>
-          <div className="flex pt-5">
-            <div className="bg-slate-500 h-8 w-8 rounded-xl"></div>
-            <div className="font-semibold text-gray-600 pl-3 translate-y-1 ">
+          <div className={` ${fold ? "justify-center" : "pt-5 justify-start pl-6"} flex cursor-pointer`} >
+            <div className= " h-8 w-8 text-gray-600 flex justify-center" >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+</svg>
+
+            </div>
+            <div className={` ${fold ? "hidden " : "flex"} font-semibold text-gray-600 pl-3 translate-y-1`} >
               Fashion Design
             </div>
           </div>{" "}
-          <div className="flex pt-5">
-            <div className="bg-slate-500 h-8 w-8 rounded-xl"></div>
-            <div className="font-semibold text-gray-600 pl-3 translate-y-1 ">
+          <div className= {`flex pt-4 ${fold ? "justify-center" : "justify-start pl-6"}   cursor-pointer`} >
+            <div className= " h-8 w-8 text-gray-600 flex justify-center" >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+</svg>
+
+            </div>
+            <div className={` ${fold ? "hidden" : "flex"} font-semibold text-gray-600 pl-3 translate-y-1`} >
               Graphic Design
             </div>
           </div>{" "}
-          <div className="flex pt-5">
-            <div className="bg-slate-500 h-8 w-8 rounded-xl"></div>
-            <div className="font-semibold text-gray-600 pl-3 translate-y-1 ">
+          <div className= {`flex pt-4 ${fold ? "justify-center" : "justify-start pl-6"} cursor-pointer`} >
+            <div className= " h-8 w-8 text-gray-600 flex justify-center" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>
+</div>
+            <div className={` ${fold ? "hidden" : "flex"} font-semibold text-gray-600 pl-3 translate-y-1`} >
               UI/UX Community
             </div>
           </div>{" "}
-          <div className="flex pt-5">
-            <div className="bg-slate-500 h-8 w-8 rounded-xl"></div>
-            <div className="font-semibold text-gray-600 pl-3 translate-y-1 ">
+          <div className= {`flex pt-4 ${fold ? "justify-center" : "justify-start pl-6"} cursor-pointer`} >
+            <div className= " h-8 w-8 text-gray-600 flex justify-center" > 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+</svg>
+            </div>
+             <div className={` ${fold ? "hidden" : "flex"} font-semibold text-gray-600 pl-3 translate-y-1`} >
               Web Designer
             </div>
           </div>
